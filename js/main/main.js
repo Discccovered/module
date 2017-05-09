d42804d775c76036d05fea04ebf6bdb19f59cf7c
Discccovered <727030944@qq.com>
2017-05-09 16:34:39 +0800

    201705091634

---
+++ js/main/main.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+$(function(){
+	$("#login").click(function(){
+		$("#myModal").modal("show");
+	})
+})
+
+function showModal(){
+	console.log("test");
+	$("#myModal").modal("show");
+}
